541e48be2472af9aa963a807d88cf333154e2d30
Pablo <pr.pablo06@gmail.com>
2025-03-25 14:21:25 +0100

    2.Saludamos dos veces en rama funciones

--- prueba_01_TuID.sh
+++ prueba_01_TuID.sh
@@ -1,7 +1,8 @@
  #!/bin/bash
  clear
  . funciones/funciones.sh
- #saludo
- echo "Soy el script 01 en la rama main."
- echo "Decidimos no saludar"
+ saludo
+ echo "Soy el script 01 en la rama funciones"
+ echo "Volvemos a saludar"
+ saludo
  exit 0
